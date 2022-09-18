464d91e36cdd24a33b793825b7f7545d5300f2a8
Gugush284 <gugush284@gmail.com>
2022-09-18 16:15:01 +0300

    Create program for lab 1.2

---
+++ LAB_1/1.2/DecodingWAV.py
@@ -0,0 +1,28 @@
+import numpy as np # импорт бибилиотеки numpy
+import matplotlib.pyplot as plt # импорт модуля matplotlib.pyplot
+import scipy.io.wavfile # импорт модуля scipy.io.wavfile
+
+from IPython.display import Audio
+Audio('glockenspiel.wav')
+
+fs, x = scipy.io.wavfile.read('glockenspiel.wav') # чтение аудиофайла
+
+print(fs)
+print(x)
+print(x.dtype)
+print(x.size)
+
+x1=x[8000:10000]                   # выбор наблюдаемого диапазона
+k=np.arange(x1.size)               # отсчеты по времени
+print(x1)
+print(k)
+
+# Построение графиков 
+plt.figure(figsize=[16, 4])        # создание полотна размером шириной 8 X 4 дюйма
+plt.plot(k/fs, x1, 'b.')           # построение графика цифрового сигнала точками точками
+plt.grid()                             
+plt.xlabel("$t$, c")                      
+plt.ylabel("$x[k]$")             
+plt.tight_layout()
+
+plt.show()
