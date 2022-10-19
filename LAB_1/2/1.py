2c982a8dcac42491834baf87a3c6ee22bd586a57
Gugush284 <gugush284@gmail.com>
2022-10-19 18:29:35 +0300

    folder refactoring

--- LAB_1/2/1.py
+++ LAB_1/2/1.py
@@ -2,6 +2,13 @@ import helpers as h
 import matplotlib.pyplot as plt  # импорт модуля matplotlib.pyplot
 import numpy as np  # импорт бибилиотеки numpy
 
+"""
+mod = 1 прямоугольное окно
+mod = 2 треугольное окно
+mod = 3 окно Ханна
+"""
+mod = 1 
+tau=1000e-6 #100 мкс
 
 def boxcar(t, tau):
     if mod == 1:
@@ -22,17 +29,6 @@ def boxcar(t, tau):
     else:
         return 0.0
 
-       
-
-"""
-mod = 1 прямоугольное окно
-mod = 2 треугольное окно
-mod = 3 окно Ханна
-"""
-mod = 3 
-tau=1000e-6 #100 мкс
-print(tau)
-
 f_band=np.linspace(-8/tau, 8/tau, 500) # 500 - число точек в диапазоне, в которых вычисляется X(f)            
     
 t_band=np.linspace(-2*tau, 2*tau, 1024)
@@ -44,12 +40,14 @@ plt.title("Сигнал")
 plt.tight_layout() 
 plt.grid()
 
+spec = h.fourier_transform(signal=boxcar, f_band=f_band, tau=tau, t1=-tau/2, t2=tau/2, res_type="abs")
+
 plt.figure(figsize=[6, 4])
-plt.plot(f_band/1e3, h.fourier_transform(signal=boxcar, f_band=f_band, tau=tau, t1=-tau/2, t2=tau/2, res_type="abs")*1e6)
+plt.plot(f_band/1e3, spec*1e6)
 plt.xlabel("Частота f, кГц")
 plt.ylabel("$|X(f)|$,  мкВ / Гц")
 plt.title("Спектр")
-plt.tight_layout() 
+plt.tight_layout()
 plt.grid()
 
 plt.show()
---
+++ LAB_1/2/2.py
@@ -0,0 +1,3 @@
+import helpers as h
+import matplotlib.pyplot as plt  # импорт модуля matplotlib.pyplot
+import numpy as np  # импорт бибилиотеки numpy
