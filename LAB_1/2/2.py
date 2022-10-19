6d17eebe0cf292f172e1cd8ef807fac3df3a96b4
Gugush284 <gugush284@gmail.com>
2022-10-19 18:33:15 +0300

    Add programm for 2.2

--- LAB_1/2/2.py
+++ LAB_1/2/2.py
@@ -1,3 +1,35 @@
 import helpers as h
 import matplotlib.pyplot as plt  # импорт модуля matplotlib.pyplot
 import numpy as np  # импорт бибилиотеки numpy
+
+tau=150*1e-6 #150 мкс
+
+f_band=np.linspace(-4/tau, 4/tau, 1000) # 1000 - число точек в диапазоне, в которых вычисляется X(f)
+
+def boxcar(t, tau):
+    if 0<t<tau:
+        return 0.1 # 0.1 В
+    else:
+        return 0.0 # 0 В
+    
+t_band=np.linspace(-2*tau, 2*tau, 1024)
+plt.figure(figsize=[8, 4])
+plt.plot(t_band*1e6, [boxcar(t, tau) for t in t_band])
+plt.xlabel("Время t, мкс")
+plt.ylabel("$x(t)$, В")
+plt.title("Сигнал")
+plt.tight_layout() 
+plt.grid()
+
+plt.figure(figsize=[8, 4])
+plt.plot(f_band/1e3, h.fourier_transform(signal=boxcar, f_band=f_band, tau=tau, t1=-2*tau, t2=2*tau, res_type="abs")*1e6, label='$|X(f)|$')
+plt.plot(f_band/1e3, h.fourier_transform(signal=boxcar, f_band=f_band, tau=tau, t1=-2*tau, t2=2*tau, res_type="Re")*1e6, label='${Re }X(f)$')
+plt.plot(f_band/1e3, h.fourier_transform(signal=boxcar, f_band=f_band, tau=tau, t1=-2*tau, t2=2*tau, res_type="Im")*1e6, label='${Im }X(f)$')
+plt.xlabel("Частота f, кГц")
+plt.ylabel("$|X(f)|$,  мкВ / Гц")
+plt.title("Спектр")
+plt.legend(loc='best', bbox_to_anchor=(1, 1))
+plt.tight_layout() 
+plt.grid()
+
+plt.show()
